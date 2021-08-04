e36e2c7d5b567da8bbe55fb848e8844b1718100f
Anurag Srivastava <53661892+StuartRyder@users.noreply.github.com>
2021-08-04 06:01:33 +0530

    added dashboard , added create page ,added signin,singup page

---
+++ pages/dashboard/index.js
@@ -0,0 +1,28 @@
+const body = document.querySelector('body')
+const cardContainer=document.querySelector('.card-container');
+const cardData=[
+    {heading:"heading1",content:"qwerty",id:1},
+    {heading:"heading2",content:"qwerty",id:2},
+    {heading:"heading3",content:"qwerty",id:3},
+    {heading:"heading4",content:"qwerty",id:4},
+    {heading:"heading5",content:"qwerty",id:5},
+    {heading:"heading6",content:"qwerty",id:6},
+    {heading:"heading7",content:"qwerty",id:7},
+];
+const createNotes=(array)=>{
+    array.forEach(cardObj => {
+        const {heading, content, id} =cardObj;
+        const card=document.createElement("div");
+        card.classList.add("card");
+        card.id=id;
+
+        const insideHtml=  `<div class="card-header"><div class="card-heading">${heading}</div><div class="edit-note"><img src="../../assets/edit.svg" alt=""></div></div><div class="card-content">${content}</div>`
+        card.innerHtml=insideHtml;
+        cardContainer.appendChild(card);
+        //console.log(`<div class="card-header"><div class="card-heading">${heading}</div><div class="edit-note"><img src="../../assets/edit.svg" alt=""></div></div><div class="card-content">${content}</div>`);
+    });
+};
+createNotes(cardData);
+window.addEventListener('load',() =>{
+    body.classList.add("visible")
+})
