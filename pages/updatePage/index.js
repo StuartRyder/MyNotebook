060b18c0dd9bd900eb2838d6887958102e94548a
Anurag Srivastava <53661892+StuartRyder@users.noreply.github.com>
2021-08-07 15:46:18 +0530

    DONE

--- pages/updatePage/index.js
+++ pages/updatePage/index.js
@@ -1,17 +1,12 @@
 const body = document.querySelector('body')
 
-window.addEventListener('load',() =>{
-    body.classList.add("visible")
-})
-
 
 const urlParams =new URLSearchParams(window.location.search);
 const noteId =urlParams.get('noteId');
 
 console.log(noteId);
 
-const body = document.querySelector('body')
-const createNoteButton =document.querySelector('.create-note-button')
+const updateNoteButton =document.querySelector('.update-note-button')
 const apiurl="https://anuragsnotebook.herokuapp.com";
 const token = localStorage.getItem("jwt");
 
@@ -20,7 +15,7 @@ updateNoteButton.addEventListener("click",()=>{
     const heading =document.querySelector('.create-note-heading').value;
 
     if(token){
-        fetch(`${apiUrl}/note/update/${noteId}`, {
+        fetch(`${apiurl}/note/update/${noteId}`, {
             method: "PUT",
             headers: {
               "Content-Type": "application/json",
@@ -44,3 +39,6 @@ updateNoteButton.addEventListener("click",()=>{
     }
 })
 
+window.addEventListener('load',() =>{
+  body.classList.add("visible")
+})
